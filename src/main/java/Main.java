ace9863094f3743cb0a961e99bdd18dd7b1baaa4
ArthurFrey <arthurfrey94@gmail.com>
2021-12-04 16:57:14 +0500

    Start

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import config.PersonalFinanceConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(PersonalFinanceConfig.class);
+
+
+
+        context.close();
+    }
+}
